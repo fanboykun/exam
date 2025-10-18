0696db09b371650c979ee9dbc9aef967fa1a4e88
fanboykun <irfanramadhan1812@gmail.com>
2025-10-18 20:09:02 +0700

    .

--- src/routes/take-exams/[examId=uuid]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { createExamState } from '$lib/hooks/exam-state.svelte.js';
-
-	let { data } = $props();
-	const examState = createExamState({ exam: data.exam });
-</script>
